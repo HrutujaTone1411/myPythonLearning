cdcbb81448ee9af085857073d2ad5155e3f2e50c
Hrutujatone <hrutujalnarale@gmail.com>
2024-06-25 12:02:32 +0530

    calculator

---
+++ calculator.py
@@ -0,0 +1,9 @@
+a=5
+b=6
+print("the value of",a,"+",3,"is:",a+b)
+print("the value of",a,"_",3,"is:",a-b)
+print("the value of",a,"*",3,"is:",a*b)
+print("the value of",a,"/",3,"is:",a/b)
+print("the value of",a,"//",3,"is:",a//b)
+print("the value of",a,"**",3,"is:",a**b)
+print("the value of",a,"++",3,"is:",a++b)
